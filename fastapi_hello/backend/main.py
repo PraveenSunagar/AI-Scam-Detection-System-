60c370f0887f9e55cbec9aa70562de65d5f12cc4
praveen p sunagar <Praveensunagar39@gmail.com>
2026-02-08 17:26:21 +0530

    Add files via upload

---
+++ fastapi_hello/backend/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# ✅ Correct way
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+
+templates = Jinja2Templates(directory="frontend/templates")
+
+# Home page
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+# Register page
+@app.get("/register")
+def register_page(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
