2c89f121334f2df102d09512063f1d9886a8b0c2
praveen p sunagar <Praveensunagar39@gmail.com>
2026-02-20 13:37:54 +0530

    add mongodb connection

--- fastapi_hello/backend/main.py
+++ fastapi_hello/backend/main.py
@@ -5,6 +5,20 @@ from backend.services.auth_service import register_user, login_user
 from pymongo import MongoClient
 from passlib.context import CryptContext
 
+app = FastAPI()
+
+# Static folder
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+
+templates = Jinja2Templates(directory="frontend/templates")
+pwd = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# MongoDB connection
+client = MongoClient("mongodb://127.0.0.1:27017")
+db = client["scam_app"]
+users = db["users"]
+
+
 
 
 
