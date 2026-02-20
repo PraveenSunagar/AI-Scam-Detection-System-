44f2e960a9130e86933ce187ad45994e910e3136
praveen p sunagar <Praveensunagar39@gmail.com>
2026-02-20 13:37:13 +0530

    Update main.py

--- fastapi_hello/backend/main.py
+++ fastapi_hello/backend/main.py
@@ -5,25 +5,7 @@ from backend.services.auth_service import register_user, login_user
 from pymongo import MongoClient
 from passlib.context import CryptContext
 
-app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
-
-templates = Jinja2Templates(directory="frontend/templates")
-
-
-@app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-
-@app.get("/register")
-def register_page(request: Request):
-    return templates.TemplateResponse("register.html", {"request": request})
-
-@app.get("/feedback")
-def feedback_page(request: Request):
-    return templates.TemplateResponse("feedback.html", {"request": request})
 
 
 
